64892cbdb6321b54197a4ed5de381f900e466d69
BirukGoshu <birukgoshu@gmail.com>
2025-12-29 22:08:58 +0300

    initial

--- Shop/models.py
+++ Shop/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
         db_table='Product'
 
 class ProductImages(models.Model):
-    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    Product=models.ForeignKey(Product,on_delete=models.CASCADE)
     images=models.ImageField(upload_to='product/')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
